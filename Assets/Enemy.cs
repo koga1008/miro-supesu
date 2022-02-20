63e9120db42cdb4c4c9bf7d4d14e747db26515db
koga1008 <95559512+koga1008@users.noreply.github.com>
2022-02-20 15:49:33 +0900

    Initial commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private bool Direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Direction == true)
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+
+            if (transform.position.x >= 8)
+            {
+                Direction = false;
+            }
+        }
+        else
+        {
+            transform.position -= new Vector3(0.1f, 0, 0);
+
+            if (transform.position.x <= -8)
+            {
+                Direction = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "bullet")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 0.9f, 0); 
+    }
+}
